57dba9f77c24a1589651c08110758824fea795e4
Winson Baring <winsonbaring10@gmail.com>
2023-04-30 09:25:03 +0800

    InvoiceItem Initial

---
+++ src/main/java/org/example/InvoiceItem.java
@@ -0,0 +1,76 @@
+package org.example;
+
+public class InvoiceItem {
+    public static void main(String[] args) {
+        // Test constructor and toString()
+        InvoiceItem inv1 = new InvoiceItem("A101", "Pen Red", 888, 0.08);
+        System.out.println(inv1);  // toString();
+
+        // Test Setters and Getters
+        inv1.setQty(999);
+        inv1.setUnitprice(0.99);
+        System.out.println(inv1);  // toString();
+        System.out.println("id is: " + inv1.getId());
+        System.out.println("desc is: " + inv1.getDesc());
+        System.out.println("qty is: " + inv1.getQty());
+        System.out.println("unitPrice is: " + inv1.getUnitprice());
+
+        // Test getTotal()
+        System.out.println("The total is: " + inv1.getTotal());
+    }
+    private String id;
+    private String desc;
+    private int qty;
+    private double unitprice;
+    public InvoiceItem(String id,String desc, int qty,double unitprice){
+        this.id = id;
+        this.desc = desc;
+        this.qty = qty;
+        this.unitprice = unitprice;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getUnitprice() {
+        return unitprice;
+    }
+
+    public void setUnitprice(double unitprice) {
+        this.unitprice = unitprice;
+    }
+    public double getTotal(){
+        return this.unitprice*(double)this.qty;
+    }
+
+    @Override
+    public String toString() {
+        return "InvoiceItem{" +
+                "id='" + id + '\'' +
+                ", desc='" + desc + '\'' +
+                ", qty=" + qty +
+                ", unitprice=" + unitprice +
+                '}';
+    }
+}
